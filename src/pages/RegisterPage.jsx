3668a6997710cd064906262d93259067b017ff0d
Reg-kun <rus>
2026-05-12 17:51:16 +0600

    l

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-// import Register from "../components/Register";
+import Register from "src/components/Register";
 
 const Registerpage = ({ studentClass, setstudentClass }) => {
   // const [message, setMessage] = useState("");
@@ -21,11 +21,6 @@ const Registerpage = ({ studentClass, setstudentClass }) => {
     return body;
   }
 
-  return (
-  <div>
-    Register Page
-  </div>
-);
-};
+return ( <div> <Register register={register} studentClass={studentClass} setstudentClass={setstudentClass} /> </div> ); };
 
 export default Registerpage;
