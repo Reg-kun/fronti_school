50455ad1c708ed9d2e1f51b7a2d3571085898efa
Reg-kun <rus>
2026-05-12 17:20:22 +0600

    i

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-// import Register from "../components/Register";
+import Register from "../components/Register";
 
 const Registerpage = ({ studentClass, setstudentClass }) => {
   // const [message, setMessage] = useState("");
